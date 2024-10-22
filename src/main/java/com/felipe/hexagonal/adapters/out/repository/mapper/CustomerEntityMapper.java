297f9386e53bc4288bafaf73c47f432d36526e5a
lipebarba2 <felipecinemkt@gmail.com>
2024-10-22 20:01:57 -0300

    Feat FindCustomerByIdOutputPort

--- src/main/java/com/felipe/hexagonal/adapters/out/repository/mapper/CustomerEntityMapper.java
+++ src/main/java/com/felipe/hexagonal/adapters/out/repository/mapper/CustomerEntityMapper.java
@@ -9,3 +9,4 @@ import org.mapstruct.Mapper;
 public interface CustomerEntityMapper {
 
     CustomerEntity toCustomerEntity(Customer customer);
+}
---
+++ src/main/java/com/felipe/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
@@ -0,0 +1,18 @@
+package com.felipe.hexagonal.application.core.usecase;
+
+import com.felipe.hexagonal.application.core.domain.Customer;
+import com.felipe.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
+
+public class FindCustomerByIdUseCase {
+
+    private final FindCustomerByIdOutputPort findCustomerByIdOutputPort;
+
+    public FindCustomerByIdUseCase(FindCustomerByIdOutputPort findCustomerByIdOutputPort) {
+        this.findCustomerByIdOutputPort = findCustomerByIdOutputPort;
+    }
+
+    public Customer find(String id) {
+        return findCustomerByIdOutputPort
+                .find(id).orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+}
